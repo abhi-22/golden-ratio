9fe13ddaccef935a2a74f78b46d766160744bc28
Abhishek singh <67219710+abhi-22@users.noreply.github.com>
2020-08-21 12:47:46 +0530

    appli

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@
             });
         }
 
-        // == Clients Slider== //
+       
         if ($('.clients-slider').length) {
             var swiper = new Swiper('.clients-slider', {
                 autoplay: {
@@ -126,12 +126,65 @@
         });
 
     });
-    function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
 })(jQuery)
+var archi=document.querySelector("#archi");
+var archi_btn=document.querySelector("#archi_btn");
+archi_btn.addEventListener("click", function(){
+    archi.style.display=" block";
+    music.style.display="none";
+appli3.style.display="none";})
+
+var archi_back_btn=document.querySelector("#archi_back_btn");
+archi_back_btn.addEventListener("click", function(){
+    archi.style.display="none";})
+
+
+    var music=document.querySelector("#music");
+var music_btn=document.querySelector("#music_btn");
+music_btn.addEventListener("click", function(){
+    music.style.display=" block";
+archi.style.display="none";
+appli3.style.display="none";})
+
+var music_back_btn=document.querySelector("#music_back_btn");
+music_back_btn.addEventListener("click", function(){
+    music.style.display="none";})
+
+var music=document.querySelector("#appli3");
+var music_btn=document.querySelector("#appli3_btn");
+appli3.addEventListener("click", function(){
+    appli3.style.display=" block";
+archi.style.display="none";
+music.style.display="none";})
+
+var appli3_back_btn=document.querySelector("#appli3_back_btn");
+appli3_back_btn.addEventListener("click", function(){
+   appli3.style.display="none";})
+
+
+    $(document).ready(function(){
+        // Add smooth scrolling to all links
+        $("a").on('click', function(event) {
+      
+          // Make sure this.hash has a value before overriding default behavior
+          if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+      
+            // Store hash
+            var hash = this.hash;
+      
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 800, function(){
+      
+              // Add hash (#) to URL when done scrolling (default click behavior)
+              window.location.hash = hash;
+            });
+          } // End if
+        });
+      });
+
+
